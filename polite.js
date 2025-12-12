877b3cf5ed48bb4930055571de45c51d43a13e80
mochikou121227-cyber <mochikou.121227@gmail.com>
2025-12-13 07:59:30 +0900

    Add files via upload

---
+++ polite.js
@@ -0,0 +1,13 @@
+export const polite=[
+  {original:'ニッポニアニッポン',romaji:'nipponia nipponn'},
+  {original:'東京特許許可局',romaji:'toukyou tokkyo kyoka kyoku'},
+  {original:'ただ暖かい',romaji:'tada atatakai'},
+  {original:'ぼろぼろの古文書',romaji:'boro boro no komonnjo'},
+  {original:'豚が豚をぶったらぶたれた',romaji:'buta ga buta wo butta ra butare ta'},
+  {original:'著作者　手術中',romaji:'tyosakusya syujutu tyuu'},
+  {original:'除雪車　除雪作業中',romaji:'josetusya josetu sagyou tyuu'},
+  {original:'赤巻紙　青巻紙　黄巻紙',romaji:'akamakigami aomakigami kimakigami'},
+  {original:'炙りカルビ',romaji:'aburi karubi'},
+  {original:'バナナの謎はまだ謎なのだぞ',romaji:'banana no nazo ha mada nazo nano da zo'},
+ 
+    ];
